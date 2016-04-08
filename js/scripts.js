9591b56367b4f3cd449df34a1f6ae60a1537f6de
TristanLobaugh <ttrader_atl@yahoo.com>
2016-04-08 16:58:06 -0400

    added zombies!
    Added a zombie that will chase the hero. Also increased the size of the
    game field.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 var gobSpeed = 1;
+var zomSpeed = 1;
 var score = 0;
 var timer = 60;
 var goblinVectorX = 0;
 var goblinVectorY = 0;
+var zombieVectorX = 0;
+var zombieVectorY = 0;
+zombies = [];
 var highScore = localStorage.getItem("highScore");
 if(typeof(highScore) === undefined){
 	highScore = 0;
@@ -14,16 +18,30 @@ if(typeof(highScore) === undefined){
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
+canvas.width = 964;
+canvas.height = 894
+;
 
-var bgImage = new Image();
-bgImage.src = "background.png";
+var bgImage1 = new Image();
+bgImage1.src = "background1.png";
+var bgImage2 = new Image();
+bgImage2.src = "background2.png";
+var bgImage3 = new Image();
+bgImage3.src = "background3.png";
+var bgImage4 = new Image();
+bgImage4.src = "background4.png";
 var hero = new Image();
 hero.src = "hero.png";
 var heroLoc ={
 	x: 240,
 	y: 224
+}
+
+var zombie = new Image();
+zombie.src = "zombie.png";
+var zombieLoc ={
+	x: 500,
+	y: 500
 }
 var goblin = new Image();
 goblin.src = "monster.png";
@@ -40,15 +58,13 @@ addEventListener("keyup", function(event){
 });
 
 function updateHero(){
-	console.log(heroLoc.x + "Hx " + heroLoc.y + "Hy " + goblinLoc.x + "Gx " + goblinLoc.y + "Gy");
-
 	if(38 in keysDown){
 		if(heroLoc.y > 30){
 			heroLoc.y -= 5;
 		}
 	}
 	if(40 in keysDown){
-		if(heroLoc.y < 415){
+		if(heroLoc.y < 829){
 			heroLoc.y += 5;
 		}
 	}
@@ -58,12 +74,11 @@ function updateHero(){
 		}
 	}
 	if(39 in keysDown){
-		if(heroLoc.x < 450){
+		if(heroLoc.x < 902){
 			heroLoc.x += 5;
 		}
 	}
 	if(heroLoc.x >= (goblinLoc.x - 20) && heroLoc.x <= (goblinLoc.x + 20) && heroLoc.y >= (goblinLoc.y - 20) && heroLoc.y <= (goblinLoc.y + 20)){
-		console.log("collision");
 		score += 1;
 		goblinLoc.x = Math.floor(Math.random()*canvas.width);
 		goblinLoc.y = Math.floor(Math.random()*canvas.height);
@@ -77,33 +92,58 @@ function updateHero(){
 	}
 }
 setInterval(function(){
-	goblinVectorX =	((Math.floor(Math.random()*4))-2)*gobSpeed;
-	goblinVectorY = ((Math.floor(Math.random()*4))-2)*gobSpeed;
+	goblinVectorX =	((Math.ceil(Math.random()*3))-2)*gobSpeed;
+	goblinVectorY = ((Math.ceil(Math.random()*3))-2)*gobSpeed;
 }, 700);
 
 function updateGoblin(){
 	goblinLoc.x += goblinVectorX;
 	goblinLoc.y += goblinVectorY;
 	if(goblinLoc.x < 16){
-		goblinLoc.x = 496;
+		goblinLoc.x = 948;
 	}
-	if(goblinLoc.x > 496){
+	if(goblinLoc.x > 948){
 		goblinLoc.x = 16;
 	}
 	if(goblinLoc.y < 16){
-		goblinLoc.y = 464;
+		goblinLoc.y = 878;
 	}
-	if(goblinLoc.y > 464){
+	if(goblinLoc.y > 878){
 		goblinLoc.y = 16;
 	}
 }
 
+setInterval(function(){
+	if(zombieLoc.x < heroLoc.x){
+		zombieVectorX =	1 * zomSpeed;
+	}else if(zombieLoc.x > heroLoc.x){
+		zombieVectorX = -1 * zomSpeed;
+	}
+
+	if(zombieLoc.y < heroLoc.y){
+		zombieVectorY =	1 * zomSpeed;
+	}else if(zombieLoc.y > heroLoc.y){
+		zombieVectorY = -1 * zomSpeed;
+	}
+	console.log(zombieVectorX + " " + zombieVectorY);
+}, 500);
+
+function updateZombie(){
+	zombieLoc.x += zombieVectorX;
+	zombieLoc.y += zombieVectorY;
+}
+
 function draw(){
-	context.drawImage(bgImage, 0, 0);
+	context.drawImage(bgImage1, 0, 0);
+	context.drawImage(bgImage2, 482, 0);
+	context.drawImage(bgImage3, 482, 447);
+	context.drawImage(bgImage4, 0, 447);
 	context.drawImage(hero, heroLoc.x, heroLoc.y);
 	context.drawImage(goblin, goblinLoc.x, goblinLoc.y);
+	context.drawImage(zombie, zombieLoc.x, zombieLoc.y);
 	updateHero();
 	updateGoblin();
+	updateZombie();
 	requestAnimationFrame(draw);
 }
 
